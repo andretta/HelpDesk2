41f1dc64cd1bfc66daf8be1680a4d894c851e217
andretta <thiago.coretto@gmail.com>
2018-05-07 15:28:34 -0300

    tualização
    atualização

--- build/generated-sources/ap-source-output/entidade/Departamento_.java
+++ build/generated-sources/ap-source-output/entidade/Departamento_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-23T22:05:23")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-05-07T14:23:23")
 @StaticMetamodel(Departamento.class)
 public class Departamento_ { 
 
